3f5c9626306afa1f5dfecb748ae5e16102982ebf
ucasvieira <lucas.souza1012@etec.sp.gov.br>
2023-03-06 08:39:30 -0300

    Lição 1

---
+++ app/src/main/java/com/example/composejetpack/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.composejetpack
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composejetpack.ui.theme.ComposeJetpackTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MessageCard("Lucas")
+        }
+    }
+}
+
+@Composable
+fun MessageCard(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard("Lucas")
+}
